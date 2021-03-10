bf16cefd3edbc5407e18dc92edbc372b7ca329c0
Shamshir-Confiz <shamshir.ali.confiz@gmail.com>
2021-03-10 08:41:05 +0500

    Initial commit

---
+++ Example/TestPod/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TestPod
+//
+//  Created by Shamshir-Confiz on 03/10/2021.
+//  Copyright (c) 2021 Shamshir-Confiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
