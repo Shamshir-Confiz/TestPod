5d5dab47dc5a32da7841f345c8c78ed0e7dafca0
Shamshir-Confiz <shamshir.ali.confiz@gmail.com>
2021-03-10 08:58:11 +0500

    init commit

--- Example/TestPod/ViewController.swift
+++ Example/TestPod/ViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
-
+import TestPod
 class ViewController: UIViewController {
-
+    var test = CustomPodTest()
     override func viewDidLoad() {
         super.viewDidLoad()
+        test.printPublicLogs()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ TestPod/Classes/CustomPodTest.swift
@@ -0,0 +1,18 @@
+//
+//  CustomPodTest.swift
+//  Pods-TestPod_Example
+//
+//  Created by Shamshir Ali on 10/03/2021.
+//
+
+import Foundation
+
+public class CustomPodTest {
+    public init() {}
+    private func printPrivateLogs() {
+        print("print private logs")
+    }
+    public func printPublicLogs() {
+        print("print public logs")
+    }
+}
